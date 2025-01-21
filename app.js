3fd4ed4cac029fbdf60227801b9d7a92ee69f29e
Asquare2003 <abdulafeezabdulazeez894@gmail.com>
2025-01-21 16:53:32 +0100

    create express server

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-console.log("It all start from localhost:3000")
-console.log("Hello API")
-console.log("Hello")
+// creating express server
+
+import express from 'express';
+
+// import bodyParser from 'body-parser';
+
+// initalize express
+const app = express();
+
+// create a port
+const PORT = 4000;
+
+// define a route
+app.get("/", (req, res) => {
+  res.send("Hello Backend Development with Expressjs")
+})
+
+app.get("/about", (req, res) =>{
+  res.send("<h2>This is About Page</h2>");
+})
+
+app.get("/home", (req, res) =>{
+  res.send("<h2>This is Home Page</h2>");
+})
+app.listen(PORT, () => {
+  console.log(`Server Is Running on port: ${PORT}`);
+})
